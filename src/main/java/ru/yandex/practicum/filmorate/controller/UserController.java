ef1adb5c6a0203d8d301dc46f44ee21fa03826de
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2024-06-07 00:42:09 +0300

    Создать шаблоны REST контроллеров

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,26 +1,26 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/films")
-public class FilmController {
+@RequestMapping("/users")
+public class UserController {
     @PostMapping
-    public Film create(@RequestBody Film film) {
+    public User create(@RequestBody User user) {
         return null;
     }
 
     @PutMapping
-    public Film update(@RequestBody Film film) {
+    public User update(@RequestBody User user) {
         return null;
     }
 
     @GetMapping
-    public Collection<Film> getAll() {
+    public Collection<User> getAll() {
         return new ArrayList<>();
     }
 }
