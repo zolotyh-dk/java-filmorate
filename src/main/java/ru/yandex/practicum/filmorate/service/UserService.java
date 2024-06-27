1574aca5695e8bf38734346395c2d0073f55b786
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2024-06-27 15:02:30 +0300

    Добавить методы удаления из друзей и получения общих друзей в UserService

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,9 +1,14 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserStorage userStorage;
@@ -14,5 +19,25 @@ public class UserService {
 
         User friend = userStorage.getUserById(friendId);
         friend.getFriendsIds().add(userId);
+    }
+
+    public void removeFriend(long userId, long friendId) {
+        User user = userStorage.getUserById(userId);
+        user.getFriendsIds().remove(friendId);
+
+        User friend = userStorage.getUserById(friendId);
+        friend.getFriendsIds().remove(userId);
+    }
+
+    public Set<Long> getCommonFriends(long userId, long otherId) {
+        User user = userStorage.getUserById(userId);
+        Set<Long> userFriendsIds = user.getFriendsIds();
+
+        User other = userStorage.getUserById(otherId);
+        Set<Long> otherFriendsIds = other.getFriendsIds();
+
+        return userFriendsIds.stream()
+                .filter(otherFriendsIds::contains)
+                .collect(Collectors.toSet());
     }
 }
